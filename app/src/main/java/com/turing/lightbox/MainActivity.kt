edafeae4dbb944d286c191227fc472a98bb508b6
Victor Pasenchuk <ucsoftworks@gmail.com>
2022-08-15 22:20:15 +0600

    update libs

--- app/src/main/java/com/turing/lightbox/MainActivity.kt
+++ app/src/main/java/com/turing/lightbox/MainActivity.kt
@@ -3,13 +3,19 @@ package com.turing.lightbox
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.flowlayout.FlowCrossAxisAlignment
+import com.google.accompanist.flowlayout.FlowMainAxisAlignment
+import com.google.accompanist.flowlayout.FlowRow
 import com.turing.lightbox.ui.theme.LightBoxTheme
 import dagger.hilt.android.AndroidEntryPoint
 
