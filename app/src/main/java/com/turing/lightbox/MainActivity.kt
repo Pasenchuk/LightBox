2b2587db4f0d9528816f15db086ff56039bc45be
Victor Pasenchuk <ucsoftworks@gmail.com>
2022-08-17 04:33:24 +0600

    orientation fix

--- app/src/main/java/com/turing/lightbox/MainActivity.kt
+++ app/src/main/java/com/turing/lightbox/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.turing.lightbox
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -11,6 +15,7 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import com.turing.controlpanel.ControlPanelUI
 import com.turing.lightbox.core.theme.LightBoxTheme
 import com.turing.mediapager.ui.MediaPagerScreen
@@ -30,6 +35,27 @@ class MainActivity : ComponentActivity() {
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
           MainUI(mainVM)
         }
+      }
+    }
+
+    hideSystemUI()
+  }
+
+  fun hideSystemUI() {
+
+    //Hides the ugly action bar at the top
+    actionBar?.hide()
+
+    //Hide the status bars
+
+    WindowCompat.setDecorFitsSystemWindows(window, false)
+
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+      window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    } else {
+      window.insetsController?.apply {
+        hide(WindowInsets.Type.statusBars())
+        systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
       }
     }
   }
